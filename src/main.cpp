24bd21989b15544b6b6c116352b3d1a1dbcd3cf1
Paul Straus <paul.straus@gmail.com>
2024-10-29 20:43:55 -0500

    saving changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,38 @@
-#include
+#include <kernel_test/kernels/add.cuh>
+
 int main()
 {
+ int N = 1 << 20;
+  float *x, *y, *out;
 
+  // Allocate Unified Memory – accessible from CPU or GPU
+  cudaMallocManaged(&x, N * sizeof(float));
+  cudaMallocManaged(&y, N * sizeof(float));
+  cudaMallocManaged(&out, N * sizeof(float));
+
+  // initialize x and y arrays on the host
+  for (int i = 0; i < N; i++)
+  {
+    x[i] = 1.0f;
+    y[i] = 2.0f;
+  }
+
+  // Run kernel on 1M elements on the GPU
+  add<<<1, 1>>>(N, x, y, out);
+
+  // Wait for GPU to finish before accessing on host
+  cudaDeviceSynchronize();
+
+  // Check for errors (all values should be 3.0f)
+  float maxError = 0.0f;
+  for (int i = 0; i < N; i++)
+    maxError = fmax(maxError, fabs(out[i] - 3.0f));
+  std::cout << "Max error: " << maxError << std::endl;
+
+  // Free memory
+  cudaFree(x);
+  cudaFree(y);
+  cudaFree(out);
+
+  return 0;
 }
